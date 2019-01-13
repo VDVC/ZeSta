f852997ef75e1469273251fc8d935aae4c466214
Patrik Schönfeldt <patrik@sedna.snft.de>
2019-01-13 15:37:23 +0100

    Erstelle Script zur Auswertung

---
+++ zesta.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+df = pd.read_csv("ZeSta.tsv", sep="\t")
+
+df.drop('Stelle', axis=1, inplace=True)
+df.drop('Beschluss', axis=1, inplace=True)
+df.drop('Titel', axis=1, inplace=True)
+
+df = df[df.Release != 2019]
+
+counts = pd.crosstab(df.Release, df.Einstufung, margins=False)
+
+freigaben = ['18',   'Cut',    'unbek.',    'A',       'B',    'Beschl.']
+farben =    ['blue', 'red', 'lightgrey', 'darkred', 'grey', 'black']
+
+counts = counts[freigaben]
+
+counts.plot(kind='bar', stacked=True, color=farben)
+
+plt.ylabel("Anzahl")
+
+plt.savefig("Zesta_2018.png")
+
