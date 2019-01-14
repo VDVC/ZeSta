b2fd9886bb2fd52cc12b4ef27b225a2f293745e5
Patrik Schönfeldt <patrik@sedna.snft.de>
2019-01-14 22:04:39 +0100

    Kleine Korrekturen

--- zesta.py
+++ zesta.py
@@ -14,13 +14,14 @@ df = df[df.Release != 2019]
 
 counts = pd.crosstab(df.Release, df.Einstufung, margins=False)
 
-freigaben = ['18',   'Cut',    'unbek.',    'A',       'B',    'Beschl.']
+freigaben = ['18',   'Cut',    'o.R.',    'A',       'B',    'Beschl.']
 farben =    ['blue', 'red', 'lightgrey', 'darkred', 'grey', 'black']
 
 counts = counts[freigaben]
 
 counts.plot(kind='bar', stacked=True, color=farben)
 
+plt.grid(axis='y')
 plt.ylabel("Anzahl")
 
 plt.savefig("Zesta_2018.png")
